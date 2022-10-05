784878dcd9847d455b8b72447b12a11a7e7ca1aa
Arjun Ramsinghani <112647362+aramsing@users.noreply.github.com>
2022-10-04 19:14:55 -0500

    Add files via upload

---
+++ src/CustomerDASH.java
@@ -0,0 +1,28 @@
+
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ *
+ * @author briao
+ */
+public class CustomerDASH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UserLanding temp = new UserLanding();
+        temp.setVisible(true);
+        
+        
+        
+        
+    }
+    
+}
